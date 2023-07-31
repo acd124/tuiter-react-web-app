299a54555d9dd5d27bf5749efdd4f9e96bb01425
AC-Dc <changdavidson4@gmail.com>
2023-07-31 18:37:28 -0400

    dynamic sizing

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -13,10 +13,10 @@ function Tuiter() {
     <div>
       <Nav />
       <div className="row">
-        <div className="col-2">
+        <div className="col-2 col-lg-1 col-xl-2">
           <NavigationSidebar />
         </div>
-        <div className="col-7">
+        <div className="col-10 col-lg-8 col-xl-7">
           <Routes>
             <Route path="/home" element={<HomeScreen />} />
             <Route path="/explore" element={<ExploreScreen />} />
@@ -24,7 +24,7 @@ function Tuiter() {
             <Route path="/profile" element={<ProfileScreen />} />
           </Routes>
         </div>
-        <div className="col-3">
+        <div className="col-3 d-none d-lg-block">
           <WhoToFollowList />
         </div>
       </div>
