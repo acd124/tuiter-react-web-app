2b60d6e43aa2f36751219077026419b7a063889f
AC-Dc <changdavidson4@gmail.com>
2023-08-15 13:31:28 -0400

    Revert "move tuit fetching further up"
    This reverts commit 45aa2dc86458bf635349affefd0d5f4cd86bbdef.

--- src/tuiter/tuit-list/index.js
+++ src/tuiter/tuit-list/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TuitItem from './tuit-item';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { findTuitsThunk } from '../services/tuits-thunks';
 
 const TuitList = () => {
   const { tuits, loading } = useSelector(state => state.tuits);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(findTuitsThunk());
+  }, []);
 
   return (
     <ul className="list-group">
