6d2e81885310c7d0cac4c0a05e2067e5821639ef
AC-Dc <changdavidson4@gmail.com>
2023-08-15 13:20:15 -0400

    fetch tuits in explore

--- src/tuiter/tuit-summary-list/index.js
+++ src/tuiter/tuit-summary-list/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TuitSummaryItem from './tuit-summary-item';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { findTuitsThunk } from '../services/tuits-thunks';
 
 const TuitSummaryList = () => {
   const { tuits } = useSelector(state => state.tuits);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(findTuitsThunk());
+  }, []);
 
   return (
     <ul className="list-group">
